529c0c8c05d941372ed299ec9060bb233ca4d6a4
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-07-21 02:53:40 -0700

    major visual update

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -5,11 +5,56 @@ const api = axios.create({
   baseURL: 'http://localhost:5001/api',
 });
 
-export const fetchNamesOfAllah = (script) => api.get('/list/names_of_allah', { params: { script } });
+// Helper function to convert Neo4j integers to regular numbers
+const convertIntegers = (obj) => {
+  if (typeof obj === 'object' && obj !== null) {
+    if ('low' in obj && 'high' in obj) {
+      return obj.low;
+    }
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        obj[key] = convertIntegers(obj[key]);
+      }
+    }
+  }
+  return obj;
+};
 
-export const fetchRootForName = (name, script) => api.get(`/name/${name}`, { params: { script } });
-export const fetchWords = (concept, script) => api.get(`/list/${concept}`, { params: { script } });
-export const fetchRootData = (root, script) => api.get(`/root/${root}`, { params: { script } });
-export const fetchWordData = (word, script) => api.get(`/word/${word}`, { params: { script } });
-export const fetchWordsByForm = (formId, script) => api.get(`/form/${formId}`, { params: { script } });
+// Updated fetch functions to handle Neo4j integers
+export const fetchWords = async (concept, script) => {
+  const response = await api.get(`/list/${concept}`, { params: { script } });
+  return response.data.map(item => convertIntegers(item));
+};
 
+export const fetchRootData = async (root, script) => {
+  const response = await api.get(`/root/${root}`, { params: { script } });
+  return response.data.map(item => convertIntegers(item));
+};
+
+export const fetchWordData = async (word, script) => {
+  const response = await api.get(`/word/${word}`, { params: { script } });
+  return convertIntegers(response.data);
+};
+
+export const fetchWordsByForm = async (formId, script) => {
+  try {
+    const response = await api.get(`/form/${formId}`, { params: { script } });
+    console.log('API response for form ID:', formId, response.data);
+    return response.data;
+  } catch (error) {
+    console.error('API error for fetchWordsByForm:', error);
+    throw error;
+  }
+};
+
+
+
+export const fetchNamesOfAllah = async (script) => {
+  const response = await api.get('/list/names_of_allah', { params: { script } });
+  return response.data.map(item => convertIntegers(item));
+};
+
+export const fetchRootForName = async (name, script) => {
+  const response = await api.get(`/name/${name}`, { params: { script } });
+  return convertIntegers(response.data);
+};
