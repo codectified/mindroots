42202a13fbf0f71ee2d542d583c8d95f564e27f3
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-07-26 16:48:32 -0700

    ui cleanup

--- src/components/RootRadicalSelector.js
+++ src/components/RootRadicalSelector.js
@@ -2,34 +2,22 @@ import React from 'react';
 
 const RootRadicalSelector = ({ arabicAlphabet, r1, r2, r3, setR1, setR2, setR3, handleRootRadicalChange }) => (
   <div>
-    <label>
-      R1:
-      <select value={r1} onChange={(e) => setR1(e.target.value)}>
-        <option value="">*</option>
-        {arabicAlphabet.map((letter, index) => (
-          <option key={index} value={letter}>{letter}</option>
-        ))}
-      </select>
-    </label>
-    <label>
-      R2:
-      <select value={r2} onChange={(e) => setR2(e.target.value)}>
-        <option value="">*</option>
-        {arabicAlphabet.map((letter, index) => (
-          <option key={index} value={letter}>{letter}</option>
-        ))}
-      </select>
-    </label>
-    <label>
-      R3:
-      <select value={r3} onChange={(e) => setR3(e.target.value)}>
-        <option value="">*</option>
-        {arabicAlphabet.map((letter, index) => (
-          <option key={index} value={letter}>{letter}</option>
-        ))}
-      </select>
-    </label>
-    <button onClick={handleRootRadicalChange}>Filter</button>
+    <label>ف:</label>
+    <select value={r1} onChange={(e) => setR1(e.target.value)}>
+      <option value="*">*</option>
+      {arabicAlphabet.map(letter => <option key={letter} value={letter}>{letter}</option>)}
+    </select>
+    <label>ع:</label>
+    <select value={r2} onChange={(e) => setR2(e.target.value)}>
+      <option value="*">*</option>
+      {arabicAlphabet.map(letter => <option key={letter} value={letter}>{letter}</option>)}
+    </select>
+    <label>ل:</label>
+    <select value={r3} onChange={(e) => setR3(e.target.value)}>
+      <option value="*">*</option>
+      {arabicAlphabet.map(letter => <option key={letter} value={letter}>{letter}</option>)}
+    </select>
+    <button onClick={handleRootRadicalChange}>Add Root</button>
   </div>
 );
 
