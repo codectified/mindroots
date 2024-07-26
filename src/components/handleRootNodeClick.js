b3a1e61530feab6354467c429bb4be179c599359
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-07-26 00:41:16 -0700

    fixed for rootNodeClick

--- src/components/handleRootNodeClick.js
+++ src/components/handleRootNodeClick.js
@@ -3,39 +3,39 @@ import { fetchRootData } from '../services/apiService';
 const handleRootNodeClick = async (node, script, rootData, setRootData, contextFilter) => {
   try {
     console.log('Fetching words for root ID:', node.root_id);
-    const rootId = node.root_id.low !== undefined ? node.root_id.low : node.root_id;
-    const response = await fetchRootData(rootId, script);
-    console.log('Fetched words by root:', response);
+    const rootIds = Array.isArray(node.root_id) ? node.root_id : [node.root_id];
+    const allResponses = await Promise.all(rootIds.map(rootId => fetchRootData(rootId, script)));
+    const allNewWords = allResponses.flat().map(word => ({
+      id: `word_${word.word_id}`,
+      label: script === 'both' ? `${word.arabic} / ${word.english}` : word[script],
+      ...word,
+      type: 'word'
+    }));
 
-    if (response && response.length > 0) {
-      const newNodes = [];
-      const newLinks = [];
+    let newNodes = [];
+    let newLinks = [];
 
-      response.forEach(word => {
-        const wordNode = {
-          id: `word_${word.word_id}`,
-          label: script === 'both' ? `${word.arabic} / ${word.english}` : word[script],
-          ...word,
-          type: 'word'
-        };
+    allNewWords.forEach(word => {
+      const existingNode = rootData.nodes.find(n => n.id === word.id);
+      if (!existingNode) {
+        newNodes.push(word);
+      }
+      newLinks.push({ source: node.id, target: word.id });
+    });
 
-        const existingNode = rootData.nodes.find(n => n.id === wordNode.id);
-        if (!existingNode) {
-          newNodes.push(wordNode);
-        }
-        newLinks.push({ source: node.id, target: wordNode.id });
-      });
-
-      const newData = {
-        nodes: [...rootData.nodes, ...newNodes],
-        links: [...rootData.links, ...newLinks]
-      };
-
-      console.log('New rootData after fetching root data:', newData);
-      setRootData(newData);
-    } else {
-      console.log('No data received for the clicked root');
+    // Apply context filter
+    if (contextFilter === 'corpus') {
+      newNodes = newNodes.filter(node => node.name_id);
+      newLinks = newLinks.filter(link => newNodes.find(node => node.id === link.target));
     }
+
+    const newData = {
+      nodes: [...rootData.nodes, ...newNodes],
+      links: [...rootData.links, ...newLinks]
+    };
+
+    console.log('New rootData after fetching root data:', newData);
+    setRootData(newData);
   } catch (error) {
     console.error('Error fetching data for clicked root node:', error);
   }
