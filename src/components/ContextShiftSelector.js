0e6d9dcf879ed56c7f74dd1bfb360221d609e2e8
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-07-27 00:43:41 -0700

    db redesign for multiple corpora

--- src/components/ContextShiftSelector.js
+++ src/components/ContextShiftSelector.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const ContextShiftSelector = ({ contextFilter, handleContextFilterChange }) => (
+const ContextShiftSelector = ({ contextFilter, handleContextFilterChange, corpora }) => (
   <select value={contextFilter} onChange={handleContextFilterChange}>
     <option value="lexicon">Lexicon</option>
-    <option value="corpus1">Corpus 1</option>
-    <option value="corpus2">Corpus 2</option>
+    {corpora.map(corpus => (
+      <option key={corpus.id} value={`corpus_${corpus.id}`}>{corpus.name}</option>
+    ))}
     <option value="currentRoots">Current Root(s)</option>
   </select>
 );
