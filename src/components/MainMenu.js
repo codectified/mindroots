3b67a0a489b2fe8c4c613d153caa9e279b11b3fb
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-09-01 22:45:13 -0700

    mainmenu revamp, settings fix, about fix

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook, faCog, faInfoCircle, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faCog, faInfoCircle, faNewspaper, faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import '../App.css'; // Make sure this points to your CSS file
 
 const MainMenu = () => {
   const navigate = useNavigate();
@@ -13,24 +14,28 @@ const MainMenu = () => {
   return (
     <div className="mainmenu-container">
       <h1 className="mainmenu-title">MindRoots</h1>
-      <ul className="mainmenu-list">
-        <li className="mainmenu-item" onClick={() => handleNavigation('/project-news')}>
-          <FontAwesomeIcon icon={faNewspaper} className="mainmenu-icon" />
-          Project News
-        </li>
-        <li className="mainmenu-item" onClick={() => handleNavigation('/corpus-menu')}>
-          <FontAwesomeIcon icon={faBook} className="mainmenu-icon" />
-          Corpus Library
-        </li>
-        <li className="mainmenu-item" onClick={() => handleNavigation('/settings')}>
-          <FontAwesomeIcon icon={faCog} className="mainmenu-icon" />
-          Language and Global Settings
-        </li>
-        <li className="mainmenu-item" onClick={() => handleNavigation('/about')}>
-          <FontAwesomeIcon icon={faInfoCircle} className="mainmenu-icon" />
-          What's this about?
-        </li>
-      </ul>
+      <div className="icon-grid">
+        <div className="icon-item" onClick={() => handleNavigation('/project-news')}>
+          <FontAwesomeIcon icon={faNewspaper} className="icon" />
+          <span>Project News</span>
+        </div>
+        <div className="icon-item" onClick={() => handleNavigation('/corpus-menu')}>
+          <FontAwesomeIcon icon={faBook} className="icon" />
+          <span>Corpus Library</span>
+        </div>
+        <div className="icon-item" onClick={() => handleNavigation('/settings')}>
+          <FontAwesomeIcon icon={faCog} className="icon" />
+          <span>Settings</span>
+        </div>
+        <div className="icon-item" onClick={() => handleNavigation('/about')}>
+          <FontAwesomeIcon icon={faInfoCircle} className="icon" />
+          <span>About</span>
+        </div>
+        <div className="icon-item" onClick={() => handleNavigation('/articles')}>
+          <FontAwesomeIcon icon={faFileAlt} className="icon" />
+          <span>Articles</span>
+        </div>
+      </div>
     </div>
   );
 };
