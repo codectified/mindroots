2b1938639a4208b9afe1d7957bd83d1bef5c8f7e
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-09-02 00:07:02 -0700

    minor frontend adjustments and games preview

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook, faCog, faInfoCircle, faNewspaper, faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faCog, faInfoCircle, faNewspaper, faFileAlt, faGamepad } from '@fortawesome/free-solid-svg-icons';
 import '../App.css'; // Make sure this points to your CSS file
 
 const MainMenu = () => {
@@ -23,17 +23,21 @@ const MainMenu = () => {
           <FontAwesomeIcon icon={faBook} className="icon" />
           <span>Corpus Library</span>
         </div>
-        <div className="icon-item" onClick={() => handleNavigation('/settings')}>
-          <FontAwesomeIcon icon={faCog} className="icon" />
-          <span>Settings</span>
-        </div>
-        <div className="icon-item" onClick={() => handleNavigation('/about')}>
-          <FontAwesomeIcon icon={faInfoCircle} className="icon" />
-          <span>About</span>
+        <div className="icon-item" onClick={() => handleNavigation('/games')}>
+          <FontAwesomeIcon icon={faGamepad} className="icon" />
+          <span>Games</span>
         </div>
         <div className="icon-item" onClick={() => handleNavigation('/articles')}>
           <FontAwesomeIcon icon={faFileAlt} className="icon" />
           <span>Articles</span>
+        </div>
+        <div className="icon-item settings-item" onClick={() => handleNavigation('/settings')}>
+          <FontAwesomeIcon icon={faCog} className="icon" />
+          <span>Settings</span>
+        </div>
+        <div className="icon-item about-item" onClick={() => handleNavigation('/about')}>
+          <FontAwesomeIcon icon={faInfoCircle} className="icon" />
+          <span>About</span>
         </div>
       </div>
     </div>
