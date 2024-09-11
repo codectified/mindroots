9742a71b239e24a21bb79a62af58da176bd85c1c
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-09-10 20:56:37 -0700

    style tweaks

--- src/components/InfoBubble.js
+++ src/components/InfoBubble.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Draggable from 'react-draggable';
 
 const InfoBubble = ({ definition, onClose, style }) => {
+  const nodeRef = useRef(null); // Ref for the draggable container
+
   return (
-    <Draggable>
-      <div className="info-bubble" style={style}>
-        <button className="close-button" onClick={onClose}>
+    <Draggable nodeRef={nodeRef} cancel=".close-button">
+      <div ref={nodeRef} className="info-bubble" style={style}>
+        <button
+          className="close-button"
+          onClick={onClose}
+          aria-label="Close InfoBubble"
+        >
           ×
         </button>
         <div className="info-bubble-content">
-          {definition ? (
-            <p>{definition}</p>
-          ) : (
-            <p>No definition available.</p>
-          )}
+          {definition ? <p>{definition}</p> : <p>No definition available.</p>}
         </div>
       </div>
     </Draggable>
