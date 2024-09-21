b9c5bbebe034ec9d7b4a43032d64061c14a937bf
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-09-21 14:55:34 -0500

    project-overview render

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
+import Markdown from 'markdown-to-jsx';
+
+const CustomListItem = ({ children }) => {
+  return <li style={{ fontSize: '1em', color: 'white' }}>{children}</li>;
+};
+
+const CustomList = ({ children }) => {
+  return <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>{children}</ul>;
+};
 
 const MarkdownRenderer = ({ filePath }) => {
   const [content, setContent] = useState('');
@@ -12,7 +20,16 @@ const MarkdownRenderer = ({ filePath }) => {
 
   return (
     <div className="markdown-homepage">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <Markdown
+        options={{
+          overrides: {
+            li: { component: CustomListItem },
+            ul: { component: CustomList },
+          },
+        }}
+      >
+        {content}
+      </Markdown>
     </div>
   );
 };
