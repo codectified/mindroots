2fcc8f56ac8c5866fa63098ac8870a12101fd4dd
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-09-22 20:40:17 -0500

    nodes shades implemented for nouns verbs phrases and other needs manual revision

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -14,31 +14,37 @@ const MainMenu = () => {
   return (
     <div className="mainmenu-container">
       <div className="icon-grid">
-      <div className="icon-item" onClick={() => handleNavigation('/corpus-menu')}>
-          <FontAwesomeIcon icon={faBook} className="icon" />
-          <span>Library</span>
-        </div>
-        <div className="icon-item" onClick={() => handleNavigation('/project-news')}>
+
+      <div className="icon-item" onClick={() => handleNavigation('/project-news')}>
           <FontAwesomeIcon icon={faNewspaper} className="icon" />
           <span>News</span>
+        </div>
+
+      <div className="icon-item" onClick={() => handleNavigation('/introduction')}>
+          <FontAwesomeIcon icon={faFileAlt} className="icon" />
+          <span>Start Here</span>
+        </div>
+
+        <div className="icon-item" onClick={() => handleNavigation('/corpus-menu')}>
+          <FontAwesomeIcon icon={faBook} className="icon" />
+          <span>Library</span>
         </div>
 
         <div className="icon-item" onClick={() => handleNavigation('/sandbox')}>
           <FontAwesomeIcon icon={faProjectDiagram} className="icon" />
           <span>Sandbox</span>
         </div>
-        <div className="icon-item" onClick={() => handleNavigation('/introduction')}>
-          <FontAwesomeIcon icon={faFileAlt} className="icon" />
-          <span>Start Here</span>
-        </div>
-        <div className="icon-item settings-item" onClick={() => handleNavigation('/settings')}>
-          <FontAwesomeIcon icon={faGlobe} className="icon" />
-          <span>Settings</span>
-        </div>
+
         <div className="icon-item about-item" onClick={() => handleNavigation('/about')}>
           <FontAwesomeIcon icon={faInfoCircle} className="icon" />
           <span>About</span>
         </div>
+
+        <div className="icon-item settings-item" onClick={() => handleNavigation('/settings')}>
+          <FontAwesomeIcon icon={faGlobe} className="icon" />
+          <span>Settings</span>
+        </div>
+
       </div>
     </div>
   );
