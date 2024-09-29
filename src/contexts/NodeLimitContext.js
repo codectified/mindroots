1668e1497db50fb3da1b00cc9701b28890299289
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-09-29 18:45:50 -0500

    minor changes

--- src/contexts/NodeLimitContext.js
+++ src/contexts/NodeLimitContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState, useContext } from 'react';
 const NodeLimitContext = createContext();
 
 export const NodeLimitProvider = ({ children }) => {
-  const [limit, setLimit] = useState(100); // Default limit
+  const [limit, setLimit] = useState(25); // Default limit
 
   return (
     <NodeLimitContext.Provider value={{ limit, setLimit }}>
