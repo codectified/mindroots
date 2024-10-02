d3c132ccf6824b2bd868d2191637335bf9ffad83
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-10-02 11:13:09 -0500

    findDOMNode depreciation

--- src/components/InfoBubble.js
+++ src/components/InfoBubble.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Draggable from 'react-draggable';
 
 const InfoBubble = ({ definition, onClose, style }) => {
+  const infoBubbleRef = useRef(null); // Create a ref for the draggable node
+
   return (
-    <Draggable cancel=".info-bubble-content, .close-button"> 
-      {/* cancel here ensures that dragging doesn't interfere with scrolling */}
-      <div className="info-bubble" style={style}>
+    <Draggable nodeRef={infoBubbleRef} cancel=".info-bubble-content, .close-button">
+      {/* Use nodeRef here to avoid findDOMNode usage */}
+      <div ref={infoBubbleRef} className="info-bubble" style={style}>
         <div className="info-bubble-header">
           <button
             className="close-button"
