a440ac2a59dea94d9a24e97bc5cd16756dd49059
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-10-03 19:14:39 -0500

    reorg

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainMenu from './components/navigation/MainMenu';
 import CorpusMenu from './components/navigation/CorpusMenu';
 import ProjectNews from './components/staticPages/ProjectNews';
-import PrimaryList from './components/staticPages/PrimaryList';
-import GraphScreen from './components/staticPages/GraphScreen';
-import Settings from './components/utils/Settings';
+import PrimaryList from './components/navigation/PrimaryList';
+import CorpusGraphScreen from './components/graph/CorpusGraphScreen';
+import Settings from './components/staticPages/Settings';
 import About from './components/staticPages/About';
-import Start from './components/staticPages/Start';
-import ArticlesList from './components/staticPages/ArticlesList';
+import Explore from './components/graph/Explore';
+import ArticlesList from './components/navigation/ArticlesList';
 import Games from './components/staticPages/Games';
 import ProjectMap from './components/staticPages/ProjectMap';
-import Sandbox from './components/staticPages/Sandbox';
+import Search from './components/graph/Search';
 
 import MarkdownRenderer from './components/utils/MarkdownRenderer';
 import DynamicMarkdownRenderer from './components/utils/DynamicMarkdownRenderer';
@@ -38,18 +38,19 @@ const App = () => {
                   <Route path="/getting-started" element={<Layout><MarkdownRenderer filePath="/mindroots/getting-started.md" /></Layout>} />
                   <Route path="/project-overview" element={<Layout><MarkdownRenderer filePath="/mindroots/project-overview.md" /></Layout>} />
                   <Route path="/elements" element={<Layout><MarkdownRenderer filePath="/mindroots/elements.md" /></Layout>} />
+                  <Route path="/about_" element={<Layout><MarkdownRenderer filePath="/mindroots/about.md" /></Layout>} />
                   <Route path="/mindroots" element={<Layout><MainMenu /></Layout>} />
                   <Route path="/corpus-menu" element={<Layout><CorpusMenu /></Layout>} />
                   <Route path="/list" element={<Layout><PrimaryList /></Layout>} />
-                  <Route path="/graph" element={<Layout><GraphScreen /></Layout>} />
+                  <Route path="/graph" element={<Layout><CorpusGraphScreen /></Layout>} />
                   <Route path="/settings" element={<Layout><Settings /></Layout>} />
                   <Route path="/project-news" element={<Layout><ProjectNews /></Layout>} />
                   <Route path="/about" element={<Layout><About /></Layout>} />
-                  <Route path="/start" element={<Layout><Start /></Layout>} />
+                  <Route path="/start" element={<Layout><Explore /></Layout>} />
                   <Route path="/articles" element={<Layout><ArticlesList /></Layout>} />
                   <Route path="/games" element={<Layout><Games /></Layout>} />
                   <Route path="/project-map" element={<Layout><ProjectMap /></Layout>} />
-                  <Route path="/sandbox" element={<Layout><Sandbox /></Layout>} />
+                  <Route path="/sandbox" element={<Layout><Search /></Layout>} />
                 </Routes>
               </Router>
             </GraphDataProvider>
