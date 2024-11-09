eb3752cfca199a1183016ea4d30518bcba83edac
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2024-11-09 11:10:28 -0600

    hide form nodes functional

--- src/components/selectors/FilterController.js
+++ src/components/selectors/FilterController.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { useFilter } from '../../contexts/FilterContext';
 
 const FilterController = () => {
-  const { filterWordTypes, toggleWordType } = useFilter();
+  const { filterWordTypes, toggleWordType, hideFormNodes, toggleHideFormNodes } = useFilter();
 
   return (
     <div style={{ display: 'flex', alignItems: 'center', gap: '20px', flexWrap: 'wrap', padding: '10px 0' }}>
-      <label>Filter by Word Type:</label>
+      <label>Filter by Nodes:</label>
 
       {['phrase', 'verb', 'noun', 'unclassified'].map(type => (
         <label key={type}>
@@ -19,6 +19,17 @@ const FilterController = () => {
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </label>
       ))}
+
+      <br></br>
+
+      <label>
+        <input
+          type="checkbox"
+          checked={hideFormNodes}
+          onChange={toggleHideFormNodes}
+        />
+        Hide Form Nodes
+      </label>
     </div>
   );
 };
