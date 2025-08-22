bac746b0eef0cb04a30adc3b4ee1ef8928d7a723
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2025-08-22 04:40:14 -0500

    CC license

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+/*
+ * MindRoots - Arabic Morphology Visualization Tool
+ * Copyright (c) 2024 MindRoots Project
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainMenu from './components/navigation/MainMenu';
