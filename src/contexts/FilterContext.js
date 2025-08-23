76d13cc4a888344c4bd95960b71a52ffcd56b0ab
Omar Ibrahim <omar.ibrahim@mcplusa.com>
2025-08-22 19:50:37 -0500

    first pass

--- src/contexts/FilterContext.js
+++ src/contexts/FilterContext.js
@@ -1,5 +1,5 @@
 // ../contexts/FilterContext.js
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import { useAdvancedMode } from './AdvancedModeContext';
 
 const FilterContext = createContext();
